70e804e5e86705a8cb0d6c739cc61d6092b8976c
Phương Nguyễn Thị Thu <phuongntt@cystack.net>
2023-08-27 22:36:35 +0700

    chore: Update locker example

--- src/LockerExample/Program.cs
+++ src/LockerExample/Program.cs
@@ -6,25 +6,14 @@ namespace LockerExample
     {
         public static void Main(string[] args)
         {
-            // var env = EnvironmentExample.GetEnvironment("newEnv");
-            // Console.WriteLine(env.Name);
-            // var modifiedEnv =
-            //     EnvironmentExample.UpdateEnvironment("newEnv", newName: "newEnv", newExternalUrl: "newExt");
-            // Console.WriteLine(modifiedEnv);
-
-            // setting by .evn file
             LockerConfiguration.Instance.Init(
                 envPath: "test.env"
             );
-            //
             var secrets = SecretExample.ListSecret();
             foreach (var secret in secrets)
             {
                 Console.WriteLine(secret);
             }
-
-            // var newEnv = EnvironmentExample.UpdateEnvironment("env2", "env2.com");
-            // Console.WriteLine(newEnv);
         }
     }
 }
