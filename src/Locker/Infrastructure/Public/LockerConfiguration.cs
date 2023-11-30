4dde18a2cd19b7e21c99893e584f30f1c643bd1d
phương nguyễn thị thu <phuongntt@cystack.net>
2023-11-30 17:01:56 +0700

    chore: Update .NET SDK

--- src/Locker/Infrastructure/Public/LockerConfiguration.cs
+++ src/Locker/Infrastructure/Public/LockerConfiguration.cs
@@ -1,9 +1,13 @@
 ﻿namespace Locker
 {
     using System.Collections.Generic;
-    using Locker.Infrastructure;
+    using Infrastructure;
     using Newtonsoft.Json;
     using DotNetEnv;
+    using System.IO;
+    using System.Security.AccessControl;
+    using System.Diagnostics;
+
 
     /// <summary>
     /// Global configuration class for Locker.net settings.
@@ -15,6 +19,9 @@
         private string _accessKeySecret;
         private string _apiVersion;
         private string _sdkVersion;
+        private string _lockerDir;
+        private string _binaryFilePath;
+        private string _binaryVersion;
         private Dictionary<string, string> _headers = new Dictionary<string, string>();
 
         private static LockerConfiguration instance;
@@ -43,6 +50,102 @@
             }
         }
 
+        private void InitBinaryPath()
+        {
+            string homeDir = System.Environment.GetFolderPath(System.Environment.SpecialFolder.UserProfile);
+            _lockerDir = Path.Combine(homeDir, ".locker");
+            _binaryVersion = "1.0.25";
+            _binaryFilePath = Path.Combine(_lockerDir, $"locker_binary-{this._binaryVersion}");
+        }
+
+        private void DownloadBinaryFile()
+        {
+            string binaryUrl;
+            var currentPlatform = System.Environment.OSVersion.Platform;
+            if (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices.OSPlatform
+                    .OSX))
+            {
+                if (System.Runtime.InteropServices.RuntimeInformation.ProcessArchitecture ==
+                    System.Runtime.InteropServices.Architecture.Arm)
+                {
+                    binaryUrl = $"https://s.locker.io/download/locker-cli-mac-arm64-{_binaryVersion}";
+                }
+                else
+                {
+                    binaryUrl = $"https://s.locker.io/download/locker-cli-mac-x64-{_binaryVersion}";
+                }
+            }
+            else if (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices
+                         .OSPlatform.Windows))
+            {
+                binaryUrl = $"https://s.locker.io/download/locker-cli-win-x64-{_binaryVersion}.exe";
+                _binaryFilePath = Path.Combine(_lockerDir, $"locker_binary-{_binaryVersion}.exe");
+            }
+            else
+            {
+                binaryUrl = $"https://s.locker.io/download/locker-cli-linux-x64-{_binaryVersion}";
+            }
+
+            // Check if the .locker directory exists, and create it if not
+            if (!Directory.Exists(_lockerDir))
+            {
+                Directory.CreateDirectory(_lockerDir);
+            }
+
+            // Download binary file
+            if (!File.Exists(_binaryFilePath))
+            {
+                using (var client = new System.Net.WebClient())
+                {
+                    Console.WriteLine($"saving to {Path.GetFullPath(_binaryFilePath)}");
+                    client.DownloadFile(binaryUrl, _binaryFilePath);
+                }
+
+                try
+                {
+                    switch (currentPlatform)
+                    {
+                        case PlatformID.Win32S:
+                        case PlatformID.Win32Windows:
+                        case PlatformID.Win32NT:
+                        case PlatformID.WinCE:
+                        {
+                            var fileInfo = new FileInfo(_binaryFilePath);
+                            var security = new FileSecurity(
+                                fileInfo.FullName,
+                                AccessControlSections.Owner |
+                                AccessControlSections.Group |
+                                AccessControlSections.Access);
+                            security.SetAccessRule(
+                                new FileSystemAccessRule(
+                                    "Everyone", FileSystemRights.ExecuteFile,
+                                    AccessControlType.Allow
+                                )
+                            );
+                            break;
+                        }
+
+                        case PlatformID.MacOSX:
+                        case PlatformID.Unix:
+                        {
+                            using Process process = new Process();
+                            process.StartInfo.FileName = "chmod";
+                            process.StartInfo.Arguments =
+                                $"755 {_binaryFilePath}";
+                            process.Start();
+                            process.WaitForExit();
+                            break;
+                        }
+                    }
+                }
+                catch (UnauthorizedAccessException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+
+
         public void Init(string apiBase = null, string accessKeyId = null, string accessKeySecret = null,
             string apiVersion = null,
             Dictionary<string, string> headers = null, string envPath = null)
@@ -52,6 +155,7 @@
             _accessKeySecret = accessKeySecret;
             _apiVersion = apiVersion;
             _headers = headers;
+
             var assembly = typeof(LockerConfiguration).Assembly;
             _sdkVersion = assembly.GetName().Version.ToString();
 
@@ -63,6 +167,9 @@
             {
                 DotNetEnv.Env.Load(".env");
             }
+
+            InitBinaryPath();
+            DownloadBinaryFile();
         }
 
 
@@ -76,7 +183,7 @@
             {
                 if (string.IsNullOrEmpty(_apiBase))
                 {
-                    _apiBase = Env.GetString("API_BASE");
+                    _apiBase = DotNetEnv.Env.GetString("API_BASE");
                 }
 
                 return _apiBase;
@@ -94,7 +201,7 @@
             {
                 if (string.IsNullOrEmpty(_accessKeyId))
                 {
-                    _accessKeyId = Env.GetString("ACCESS_KEY_ID");
+                    _accessKeyId = DotNetEnv.Env.GetString("ACCESS_KEY_ID");
                 }
 
                 return _accessKeyId;
@@ -112,7 +219,7 @@
             {
                 if (string.IsNullOrEmpty(_accessKeySecret))
                 {
-                    _accessKeySecret = Env.GetString("ACCESS_KEY_SECRET");
+                    _accessKeySecret = DotNetEnv.Env.GetString("ACCESS_KEY_SECRET");
                 }
 
                 return _accessKeySecret;
@@ -130,7 +237,7 @@
             {
                 if (string.IsNullOrEmpty(_apiVersion))
                 {
-                    _apiVersion = Env.GetString("API_VERSION");
+                    _apiVersion = DotNetEnv.Env.GetString("API_VERSION");
                 }
 
                 return _apiVersion;
@@ -142,8 +249,8 @@
         {
             get
             {
-                var cfAccessClientId = Env.GetString("CF_ACCESS_CLIENT_ID");
-                var cfAccessClientSecret = Env.GetString("CF_ACCESS_CLIENT_SECRET");
+                var cfAccessClientId = DotNetEnv.Env.GetString("CF_ACCESS_CLIENT_ID");
+                var cfAccessClientSecret = DotNetEnv.Env.GetString("CF_ACCESS_CLIENT_SECRET");
                 _headers = _headers == null || _headers.Count == 0
                     ? new Dictionary<string, string>()
                     {
@@ -153,6 +260,7 @@
                     : _headers;
                 return _headers;
             }
+
             set => _headers = value;
         }
 
@@ -160,6 +268,16 @@
         {
             get => this._sdkVersion;
             set => _sdkVersion = value;
+        }
+
+        public string LockerDir
+        {
+            get => _lockerDir;
+        }
+
+        public string BinaryFilePath
+        {
+            get => _binaryFilePath;
         }
 
 
