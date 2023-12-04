62c7d043211e018ee1100d879915451d5ad3994b
phương nguyễn thị thu <phuongntt@cystack.net>
2023-12-04 09:42:56 +0700

    chore: Update deploy.py

--- src/deploy.py
+++ src/deploy.py
@@ -4,4 +4,5 @@ import xmltodict
 with open("Locker/Locker.csproj") as xml_file:
     data_dict = xmltodict.parse(xml_file.read())
     version = data_dict['Project']['PropertyGroup']['Version']
-    os.system(f'nuget push Locker\\bin\\Release\\locker-secrets.{version}.nupkg')
+    package_id = data_dict['Project']['PropertyGroup']['PackageId']
+    os.system(f'nuget push Locker\\bin\\Release\\{package_id}.{version}.nupkg')
