2e3d067dae418ac5782271316347f73a77b65f31
manhtx <manhtx@cystack.net>
2024-10-30 18:14:44 +0700

    update-cicd

--- src/deploy.py
+++ src/deploy.py
@@ -5,4 +5,7 @@ with open("Locker/Locker.csproj") as xml_file:
     data_dict = xmltodict.parse(xml_file.read())
     version = data_dict['Project']['PropertyGroup']['Version']
     package_id = data_dict['Project']['PropertyGroup']['PackageId']
+    os.system(f'nuget setApiKey {os.getenv("NUGET_API_KEY")}')
     os.system(f'nuget push Locker\\bin\\Release\\{package_id}.{version}.nupkg')
+
+
