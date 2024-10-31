f910e7fc1e8c9acd594cdb96c680529b80b9e49d
manhtx <manhtx@cystack.net>
2024-10-31 16:21:06 +0700

    update

--- src/deploy.py
+++ src/deploy.py
@@ -5,7 +5,8 @@ import subprocess
 with open("Locker/Locker.csproj") as xml_file:
     data_dict = xmltodict.parse(xml_file.read())
     version = data_dict['Project']['PropertyGroup']['Version']
-    
+    package_id = data_dict['Project']['PropertyGroup']['PackageId']
+
     subprocess.run(['nuget', 'setApiKey'], input=os.getenv('NUGET_API_KEY'), text=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
-    os.system(f'nuget push Locker\\bin\\Release\\locker-secrets.{version}.nupkg')
+    os.system(f'nuget push Locker\\bin\\Release\\{package_id}.{version}.nupkg')
